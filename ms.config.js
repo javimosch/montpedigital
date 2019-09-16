5ceb0e06a4c1de0dd2f804342ba62629c0c213e0
Javi <arancibiajav@gmail.com>
2019-09-16 16:33:47 +0200

    config updated

--- ms.config.js
+++ ms.config.js
@@ -1,5 +1,9 @@
 module.exports = async app => {
     return {
+        env: {
+            defaultLanguage: 'fr'
+        },
+        layoutPartials: ['_footer.html', '_head.html'],
         plugins: {
             cleanPublicHtmlFolders: {
                 preserveFolders: ['img']
