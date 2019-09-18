52968b49ece63f4a7034d847005ea86a4638c857
Javier Leandro Arancibia <arancibiajav@gmail.com>
2019-09-18 13:47:55 +0000

    about page added

--- src/locales.js
+++ src/locales.js
@@ -4,15 +4,17 @@ module.exports = async options => {
             MAIN_PAGE: 'Main Page',
             LEGAL_MENTIONS: 'Legal Mentions',
             SEND: 'Send',
-            CONTACT_MESSAGE: 'Available on WhatsApp',
-            SITEMAP: 'Sitemap'
+            CONTACT_MESSAGE: 'Available also on WhatsApp',
+            SITEMAP: 'Sitemap',
+            HQ_ADDRESS: "HQ Address"
         },
         fr: {
             MAIN_PAGE: "Page d'accueil",
             LEGAL_MENTIONS: 'Mentions Légales',
             SEND: 'Envoyer',
             CONTACT_MESSAGE: 'Disponible sur WhatsApp',
-            SITEMAP: 'Plan du site'
+            SITEMAP: 'Plan du site',
+            HQ_ADDRESS: "Addrese QG"
         }
     }
 }
---
+++ src/pages/about-me/index.js
@@ -0,0 +1,9 @@
+module.exports = async app => {
+    return {
+        layout: 'page',
+        head_title: 'About',
+        title: `About Javier Leandro Arancibia`,
+        target: '/about-javier-leandro-arancibia',
+        meta_keywords: `about information developer web javascript javier leandro arancibia`
+    }
+}
