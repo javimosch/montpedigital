0fb9e512903c7a815c6d33bc207a7ad20439a3a9
Javi <arancibiajav@gmail.com>
2019-09-23 12:17:26 +0200

    UX changes

---
+++ src/pages/article-quitter-google-experience-framasoft/index.js
@@ -0,0 +1,9 @@
+module.exports = async app => {
+    return {
+        layout: 'blog',
+        head_title: `Quitter Google ? L'expérience de l'association Framasoft`,
+        title: `Quitter Google ? L'expérience de l'association Framasoft`,
+        target: `/quitter-google-la-experience-de-la-asso-framasoft`,
+        meta_keywords: `google framasoft association france`
+    }
+}
